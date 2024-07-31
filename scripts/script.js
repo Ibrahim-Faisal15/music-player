1b3f0a3cba4e7b9b2427840c6e80803b3cc4bbae
M Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2024-08-01 04:12:50 +0500

    *

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const audioContext = new AudioContext();
+  const audioContext = new Audio();
 
   let music_playlist = document.querySelector(".music_playlist");
-
   let playlist = [];
-
   let audio_chooser = document.querySelector(".file_chooser");
+  // Creating Playlist Items
+  let play = document.querySelector(".play");
+  let forward = document.querySelector(".forward");
+  let rewind = document.querySelector(".rewind");
+  let seek_bar = document.querySelector(".progress_bar");
 
   audio_chooser.addEventListener("change", (e) => {
     let play_list_item = document.createElement("div");
     Array.from(audio_chooser.files).forEach((file) => {
       playlist.push(file);
       playlist.forEach((i) => {
-        play_list_item.innerText = i.name.split(".mp3")[0];
+        audio_URL = URL.createObjectURL(i);
+        play_list_item.innerText = i.name.split(".")[0];
         play_list_item.style.height = "52px";
         play_list_item.style.backgroundColor = "#2c3e50";
         play_list_item.style.width = "90%";
@@ -25,10 +29,23 @@ document.addEventListener("DOMContentLoaded", () => {
         play_list_item.style.alignItems = "center";
         play_list_item.style.color = "white";
         play_list_item.style.paddingLeft = "23px";
+        play_list_item.className = "indie_audio";
         music_playlist.appendChild(play_list_item);
+      });
 
-        music_playlist.addEventListener("click", () => {
-          console.log(23);
+      document.querySelector(".indie_audio").addEventListener("click", () => {
+        audioContext.src = audio_URL;
+        console.log(audioContext.currentSrc);
+        audioContext.play();
+        play.src = "SVG/pause.svg";
+        play.addEventListener("click", () => {
+          if (audioContext.paused) {
+            audioContext.play();
+            play.src = "SVG/play.svg";
+          } else {
+            audioContext.pause();
+            play.src = "SVG/pause.svg";
+          }
         });
       });
     });
